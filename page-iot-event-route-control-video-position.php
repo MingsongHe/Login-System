93a5f344ef587138b0f85efb4a17055abd86698a
HE MINGSONG <122031452+MingsongHe@users.noreply.github.com>
2023-01-07 20:55:27 +0800

    Update page-iot-event-route-control-video-position.php

--- page-iot-event-route-control-video-position.php
+++ page-iot-event-route-control-video-position.php
@@ -40,8 +40,7 @@ else {
 
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- 提高电脑，手机 移动装置显示位置兼容性 -->
-   
-
+ 
 <style>
 .img-video
   {  
@@ -51,6 +50,7 @@ else {
   border-radius:6px;
   max-width:85%;
   margin: auto;
+  position:relative;top:80px;
  }
  .box{
     margin: auto;
@@ -58,55 +58,25 @@ else {
     border: 1px solid #73AD21;
     padding: 8px;
  }
- .box2{
-    margin:auto;
-    width:500px;
-    height:544px;
-}
-
-#setting-01 {
-  width: 20%;
-}
-#setting-02 {
-  width: 15%;
-}
-#setting-03 {
-  width: 10%;
-}
-#setting-04 {
-  width: 40%;
-}
-#setting-05 {
-  width: 40%;
-}
-#setting-01,#setting-02,#setting-03,#setting-04,#setting-05{
-    float: left;  /*向左浮动*/
-    height: 110px;
-}
-#setting-04,#setting-05{
-    float: left;  /*向左浮动*/
-    height: 60px;
-}
-
 </style>
 
 </head>
 
 <body>
 <div class="img-video">
-        <div id= "iot-container" class="box" style="position:relative;top:1px;">    	      
+        <div id= "iot-container" class="box">    	      
             <video id="media" width="100%" height="100%" controls autoplay>
                 <source id="video" src="https://bilingualplan.com/wp-content/uploads/2022/12/Overture-荒野大鏢客主題曲.mp4" type="video/mp4">
             </video>
         </div>
-</div>
         <div style="text-align: center; position:relative;top:1px">
           <button id="m1" class="btn btn-default">第一位置</button>
           <button id="m2" class="btn btn-default">第二位置</button>
           <button id="m3" class="btn btn-default">第三位置</button>
           <button id="m4" class="btn btn-default">第四位置</button>
           <button id="m5" class="btn btn-default">第五位置</button>
-        </div>     
+        </div>
+</div>    
 </body>
 
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script> <!--没有这个文件，JavaScript 功能可能失效-->
